0aa37f7cf38f1b0d7cf9822fd7c7ba10fe03e876
Mohamed Salah Hassan Akel <sci.m.s.hassan@hotmail.com>
2021-01-11 19:54:01 +0200

    Web Scraping Projects Init

---
+++ scraping_hacker_news/HackerNewsDataCollecting/HackerNewsDataCollecting.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 18:59:30 2021
+
+@author: mohamed_akel
+"""
+
+import requests               # simple http library for python
+import re                     # regexp python library
+from bs4 import BeautifulSoup # Python library for pulling data out of HTML and XML files
